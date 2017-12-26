0cd72c8ba8415e8ba5480c769fb2c4cbb6f3b548
pmattiollo <pmattiollo@gmail.com>
2017-12-26 21:29:20 -0200

    Alteração a estrutura dos diretórios

--- eventos/disparando-eventos/disparando-eventos.js
+++ eventos/disparando-eventos/disparando-eventos.js
@@ -1,22 +1,18 @@
-$(function() {
+$(function () {
+
 	var atividade = $('#atividade');
 	var listaAtividades = $('#lista-atividades');
 
+	
 	atividade.on('keyup', function(e) {
-		var keyCode = e.keyCode;
-
-		if (keyCode === 13) {
+		if (e.keyCode == 13) {
 			atividade.trigger('enter', atividade.val());
 			atividade.val('');
 		}
 	});
 
-	atividade.on('enter', function(evento, texto) {
-		listaAtividades.prepend('<li>' + texto + '</li>');
+	atividade.on('enter', function(evento, valor) {
+		listaAtividades.prepend('<li>' + valor + '</li>');
 	});
-
-	atividade.on('enter', function(eventp, texto) {
-		console.log('Outra funcionalidade aqui...');
-	});
-
+	
 });
